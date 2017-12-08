3e7fb3735ba0168f3c7d671ded8b5f887d862502
Emiliano Tisato <emilianotisato@gmail.com>
2017-12-08 05:48:41 -0300

    Added database config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,11 +7,38 @@ use EmilianoTisato\LaravelInstaller\LaravelInstallerServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
+    /**
+     * Setup the test environment.
+     */
     public function setUp()
     {
         parent::setUp();
+
+        $this->loadLaravelMigrations(['--database' => 'testing']);
     }
 
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing');
+    }
+
+    /**
+     * Get package providers.  At a minimum this is the package being tested, but also
+     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
+     * In a normal app environment these would be added to the 'providers' array in
+     * the config/app.php file.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
     protected function getPackageProviders($app)
     {
         return [LaravelInstallerServiceProvider::class];
